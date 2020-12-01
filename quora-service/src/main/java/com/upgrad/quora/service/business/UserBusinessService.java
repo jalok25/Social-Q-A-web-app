ec33ba13246928e94a30ced15d231aa5f7d6c12c
Spandana Pokla <spandana.pokla@hcl.com>
2020-12-01 22:12:55 +0530

    modified changes

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -1,4 +1,53 @@
 package com.upgrad.quora.service.business;
 
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class UserBusinessService {
+
+    @Autowired
+    private AdminBusinessService adminBusinessService;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
+        return adminBusinessService.createUser(userEntity);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity signout(final String authorizationToken) throws SignOutRestrictedException {
+        return adminBusinessService.logoutUser(authorizationToken);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity getUser(final String userId, final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuthToken(authorization);
+
+        // Validate if user is signed in or not
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        // Validate if user has signed out
+        if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
+        }
+
+        // Validate if requested user exist or not
+        UserEntity userEntity = userDao.getUserByUuid(userId);
+        if (userEntity == null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
+        }
+
+        return userEntity;
+    }
 }
